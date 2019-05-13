9702daeb843434422865dd65bd6242d3a46221eb
Troy Dean <tald2@students.waikato.ac.nz>
2019-05-13 14:47:52 +1200

    Vague skeleton for Dequeue

---
+++ Dequeue.java
@@ -0,0 +1,22 @@
+public class Dequeue{
+    private class node{
+        Object value;
+        node next;
+    }
+
+    public Dequeue() {
+        super();
+    }
+
+    public void push(){
+
+    }
+
+    public void pop(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Dequeue");
+    }
+}
