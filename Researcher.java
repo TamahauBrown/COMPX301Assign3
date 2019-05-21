fccfc0ce43a6d64c4250e3c631a9ee70721323a8
Troy Dean <tald@xtra.co.nz>
2019-05-21 21:15:16 +1200

    Build list from compiler output Added test script

--- Researcher.java
+++ Researcher.java
@@ -14,25 +14,37 @@ public class Researcher {
 
     public Researcher(){
         root = new FSMNode();
+        root.index = -1;
     }
 
     public void addFSMNode(FSMNode node){
-        root = addFSMNode(node, 0);
+        root = addFSMNode(root, node, 0);
     }
 
-    private FSMNode addFSMNode(FSMNode node, int i) {
-        return null;
+    private FSMNode addFSMNode(FSMNode currentNode,  FSMNode newNode, int i) {
+        if(currentNode == null){
+            newNode.index = i;
+            return newNode;
+        } 
+        currentNode.next = addFSMNode(currentNode.next, newNode, ++i);
+        return currentNode;
     }
 
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
             String input;
-
+            Researcher r = new Researcher();
             while(scanner.hasNextLine()){
                 input = scanner.nextLine();
+                input = input.trim();
                 String[] fsmLine = input.split(" ", 4);
-
+                
+                FSMNode newNode = r.new FSMNode();
+                newNode.ch = fsmLine[1];
+                newNode.nextState1 = Integer.parseInt(fsmLine[2]);
+                newNode.nextState2 = Integer.parseInt(fsmLine[3]);
+                r.addFSMNode(newNode);
                 System.out.println(input);
                 
 
