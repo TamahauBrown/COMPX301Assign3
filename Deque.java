006958ce4a1bf4d043e1366c113b8f7934e1967b
Troy Dean <tald@xtra.co.nz>
2019-05-22 22:46:42 +1200

    Added some comments

--- Deque.java
+++ Deque.java
@@ -1,20 +1,24 @@
 public class Deque{
+    //Node to represent values in the Deque
     private class Node{
         Object value;        
         Node next;
     }
 
+    // Handles for the root, scan and tail of the deque for easier management
     Node root;
     Node scan;
     Node tail;
 
     public Deque() {
+        // Create the Deque with the SCAN node in place and set up handles
         scan = new Node();
         scan.value = "SCAN";
         root = scan;
         tail = scan;
     }
 
+    // Push a value on to the stack
     public void push(Object val){
         Node newRoot = new Node();
         newRoot.value = val;
@@ -22,6 +26,7 @@ public class Deque{
         root = newRoot;
     }
 
+    //Pop a value off of the stack or return null if the SCAN item is detected
     public Object pop(){
         if(root.value == "SCAN"){
             return null;
@@ -31,6 +36,7 @@ public class Deque{
         return val;
     }
 
+    // Put a value into the queue
     public void put(Object val){
         tail.next = new Node();
         tail = tail.next;
--- Researcher.java
+++ Researcher.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Researcher {
+    // A node for representing an FSM state
     private class FSMNode {
         int index;
         String ch;
@@ -18,14 +19,17 @@ public class Researcher {
     Deque deque = new Deque();
 
     public Researcher() {
+        //Create a root node to build the list of states
         root = new FSMNode();
         root.index = -1;
     }
 
-    public void addFSMNode(FSMNode node) {
+    // Add node to list off states
+    public void addFSMNode(FSMNode node) {        
         root = addFSMNode(root, node);
     }
 
+    // Add new node at tail of list of states
     private FSMNode addFSMNode(FSMNode currentNode, FSMNode newNode) {
         if (currentNode == null) {
             return newNode;
@@ -34,10 +38,12 @@ public class Researcher {
         return currentNode;
     }
 
-    private FSMNode get(int index) {
+    // Get a node withthe index
+    public FSMNode get(int index) {
         return get(root, index);
     }
 
+    // Find and return the correct node or null if that index is not found
     private FSMNode get(FSMNode n, int index) {
         while (n != null && n.index != index && n.index <= index) {
             n = n.next;
@@ -50,22 +56,28 @@ public class Researcher {
 
     public void readFSM() {
         try {
+            // Read output of REcompiler line by line from std in
             Scanner scanner = new Scanner(System.in);
             String input;
 
             while (scanner.hasNextLine()) {
                 input = scanner.nextLine();
                 input = input.trim();
-                if (!input.contains("EXIT")) {                    
+                //Look for exit message on compiler output
+                if (!input.contains("EXIT")) {            
+                    // Split string into components        
                     String[] fsmLine = input.split(" ", 4);
 
+                    //Create a new node to represnt the state being read
                     FSMNode newNode = new FSMNode();
                     newNode.index = Integer.parseInt(fsmLine[0]);
                     newNode.ch = fsmLine[1];
                     newNode.nextState1 = Integer.parseInt(fsmLine[2]);
                     newNode.nextState2 = Integer.parseInt(fsmLine[3]);
+
+                    //Add the new state node to the list
                     addFSMNode(newNode);
-                    System.out.println(input);
+                    
                 }
 
             }
@@ -79,43 +91,52 @@ public class Researcher {
     }
 
     public void searchLine(String line) {
+        // set current state to 0
         int currentState = 0;
         for (int i = 0; i < line.length(); i++) {
+            //work through the line to be checked character by character
             boolean match = false;
+            //Get the current state and push it to the stack
             deque.push(get(currentState));
 
+            //Get the current state from the stack
             FSMNode state = (FSMNode) deque.pop();
 
+            //if the state is null a match has been found (all states traversed)
             if (state == null) {
                 System.out.println(line);
                 return;
             }
-            //System.out.println(state.ch);
-            if(state.ch.contains(".")){
-                //System.out.println("here");
+            
+            // Handle . operator
+            if(state.ch.contains(".")){            
                 currentState = state.nextState1;
                 deque.push(get(currentState));
                 state = (FSMNode) deque.pop();
             }
-            // System.out.println(state.ch.charAt(0) + ": " + line.charAt(i));
+            // Handle non special operators
             if (!isSpecial(state.ch)) {
                 if (state.ch.charAt(0) == line.charAt(i)) {
                     match = true;
-                    deque.put(get(state.nextState1));
-                    // System.out.println(state.ch.charAt(0) + ": " + line.charAt(i));
+                    deque.put(get(state.nextState1));                    
                 }
             } else {
+                // Handle branch states
                 deque.push(get(state.nextState1));
-                deque.push(get(state.nextState1));
+                deque.push(get(state.nextState2));                
             }
 
             if (match) {
+                // If a match is found move to the next character and state
                 currentState = state.nextState1;
                 if (i == line.length() - 1 && get(currentState) == null) {
+                    // If this is the last character in the string, check if the state list has been exhausted
+                    // if so this is a match 
                     System.out.println(line);
                     return;
                 }
             } else {
+                // If character was not a match reset current state and move to the next character in the string
                 currentState = 0;
             }
         }
@@ -130,19 +151,18 @@ public class Researcher {
     }
 
     public void searchFile(String fname) {
+        //Read a file in line by line
         File file = new File(fname);
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String s;
-            while ((s = br.readLine()) != null) {
-                // System.out.println(s);
+            while ((s = br.readLine()) != null) {                
+                //Search each line for at least 1 match ogf the regex
                 searchLine(s);
             }
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
+        } catch (FileNotFoundException e) {            
             System.out.println("File " + fname + " not found");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+        } catch (IOException e) {            
             e.printStackTrace();
         }
     }
